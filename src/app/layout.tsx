abe902e3ddcdbee048865f25e2845e18173eb166
Nisharg Shah <nishargshah3101@gmail.com>
2023-10-29 20:38:42 +0530

    feat: converted into next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from 'next';
+import { Lato } from 'next/font/google';
+import './globals.css';
+
+const lato = Lato({
+  subsets: ['latin', 'latin-ext'],
+  style: 'normal',
+  weight: ['400', '900'],
+});
+
+export const metadata: Metadata = {
+  title: 'Node Ahmedabad',
+  description:
+    'Node Ahmedabad is a community that helps developers to understand the Node programming language and their respective frameworks.',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossOrigin="anonymous"
+        />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+      </head>
+      <body className={lato.className}>
+        {children}
+        <script
+          async
+          src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+          integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+          crossOrigin="anonymous"
+        />
+        <script
+          async
+          src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+          integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+          crossOrigin="anonymous"
+        />
+        <script
+          async
+          src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+          integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+          crossOrigin="anonymous"
+        />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import UserIcon from '@/components/UserIcon';
+
+export default function Home() {
+  return (
+    <Fragment>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10 main-title">
+            <h1>Website</h1>
+            <h2>under construction</h2>
+          </div>
+        </div>
+        <div className="row align-items-center">
+          <div className="col-sm-10 col-md-6 col-lg-6 svg-img p-5">
+            <UserIcon />
+          </div>
+        </div>
+      </div>
+      <footer className="footer">
+        <div className="container text-center">
+          <span>nodeahmedabad.com</span>
+        </div>
+      </footer>
+    </Fragment>
+  );
+}
