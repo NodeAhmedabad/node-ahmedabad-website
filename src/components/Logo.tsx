357201dd91224f90542d74fc2f04b1b5867c814b
Nisharg Shah <nishargshah3101@gmail.com>
2025-10-27 23:09:09 +0530

    fix: logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,71 +1,71 @@
-"use client"
-import React, { useRef } from "react"
-import Image from "next/image"
-import n from "@/assets/n.webp"
-import o from "@/assets/o.webp"
-import d from "@/assets/d.webp"
-import e from "@/assets/e.webp"
+'use client'
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import nodeN from '@/assets/node/n.webp'
+import nodeO from '@/assets/node/o.webp'
+import nodeD from '@/assets/node/d.webp'
+import nodeE from '@/assets/node/e.webp'
 
-import A from "@/assets/A.webp"
-import H from "@/assets/H.webp"
-import M from "@/assets/M.webp"
-import E from "@/assets/E.webp"
-import D from "@/assets/D.webp"
-import B from "@/assets/B.webp"
+import ahmedabadA from '@/assets/ahmedabad/a.webp'
+import ahmedabadH from '@/assets/ahmedabad/h.webp'
+import ahmedabadM from '@/assets/ahmedabad/m.webp'
+import ahmedabadE from '@/assets/ahmedabad/e.webp'
+import ahmedabadD from '@/assets/ahmedabad/d.webp'
+import ahmedabadB from '@/assets/ahmedabad/b.webp'
 
-import bannerBg from "@/assets/bg.webp"
+import bannerBg from '@/assets/bg.webp'
 
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const Logo = () => {
   const bannerBgRef = useRef<HTMLImageElement>(null)
 
   const node = [
     <Image
-      key="n"
-      src={n}
-      alt="Node.js"
-      className="node-img"
+      key='n'
+      src={nodeN}
+      alt='Node.js'
+      className='node-img'
       style={{
-        margin: "0 10px",
-        width: "100px",
+        margin: '0 10px',
+        width: '100px',
         opacity: 0,
       }}
     />,
     <Image
-      key="o"
-      src={o}
-      alt="Node.js"
-      className="node-img"
+      key='o'
+      src={nodeO}
+      alt='Node.js'
+      className='node-img'
       style={{
-        transform: "translateY(10px)",
-        margin: "0 10px",
-        width: "100px",
+        transform: 'translateY(10px)',
+        margin: '0 10px',
+        width: '100px',
         opacity: 0,
       }}
     />,
     <Image
-      key="d"
-      src={d}
-      alt="Node.js"
-      className="node-img"
+      key='d'
+      src={nodeD}
+      alt='Node.js'
+      className='node-img'
       style={{
-        transform: "translateY(10px)",
-        margin: "0 10px",
-        width: "100px",
+        transform: 'translateY(10px)',
+        margin: '0 10px',
+        width: '100px',
         opacity: 0,
       }}
     />,
     <Image
-      key="e"
-      src={e}
-      alt="Node.js"
-      className="node-img"
+      key='e'
+      src={nodeE}
+      alt='Node.js'
+      className='node-img'
       style={{
-        transform: "translateY(10px)",
-        margin: "0 10px",
-        width: "100px",
+        transform: 'translateY(10px)',
+        margin: '0 10px',
+        width: '100px',
         opacity: 0,
       }}
     />,
@@ -73,107 +73,107 @@ const Logo = () => {
 
   const ahmedabad = [
     <Image
-      key="A"
-      src={A}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='A'
+      src={ahmedabadA}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="H"
-      src={H}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='H'
+      src={ahmedabadH}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="M"
-      src={M}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='M'
+      src={ahmedabadM}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
-        minWidth: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
+        minWidth: 'fit-content',
       }}
     />,
     <Image
-      key="E"
-      src={E}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='E'
+      src={ahmedabadE}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="D"
-      src={D}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='D'
+      src={ahmedabadD}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
 
     <Image
-      key="A"
-      src={A}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='A'
+      src={ahmedabadA}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="B"
-      src={B}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='B'
+      src={ahmedabadB}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="A"
-      src={A}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='A'
+      src={ahmedabadA}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
     <Image
-      key="D"
-      src={D}
-      alt="Ahmedabad"
-      className="ahmedabad-img"
+      key='D'
+      src={ahmedabadD}
+      alt='Ahmedabad'
+      className='ahmedabad-img'
       style={{
-        transform: "translateY(300px)",
-        flexBasis: "fit-content",
+        transform: 'translateY(300px)',
+        flexBasis: 'fit-content',
       }}
     />,
   ]
 
   const animateSlideUp = () => {
-    const images = document.querySelectorAll(".ahmedabad-img")
+    const images = document.querySelectorAll('.ahmedabad-img')
     const tl = gsap.timeline({
       delay: 0.5,
     })
     for (const img of Array.from(images)) {
-      const y = Number(img.getAttribute("height")) * 2
-      console.log("height:", y)
+      const y = Number(img.getAttribute('height')) * 2
+      console.log('height:', y)
       tl.fromTo(
         img,
         {
@@ -183,9 +183,9 @@ const Logo = () => {
           y: 0,
           delay: 0.5,
           duration: 0.8,
-          ease: "power2.out",
+          ease: 'power2.out',
         },
-        "-=1.1"
+        '-=1.1'
       )
     }
   }
@@ -194,12 +194,12 @@ const Logo = () => {
     gsap.fromTo(
       bannerBgRef.current,
       {
-        clipPath: "inset(0% 0% 100% 0%)",
+        clipPath: 'inset(0% 0% 100% 0%)',
       },
       {
-        clipPath: "inset(0% 0% 0% 0%)",
+        clipPath: 'inset(0% 0% 0% 0%)',
         duration: 1,
-        ease: "power2.out",
+        ease: 'power2.out',
         delay: 0.5,
         onComplete: () => {
           animateSlideUp()
@@ -209,11 +209,11 @@ const Logo = () => {
   }
 
   const animateFadeIn = () => {
-    const images = document.querySelectorAll(".node-img")
+    const images = document.querySelectorAll('.node-img')
     const tl = gsap.timeline({
       delay: 0.5,
       onComplete: () => {
-        console.log("tl complete")
+        console.log('tl complete')
         animateBannerReveal()
       },
     })
@@ -227,9 +227,9 @@ const Logo = () => {
           opacity: 1,
           delay: 0.5,
           duration: 0.8,
-          ease: "power2.out",
+          ease: 'power2.out',
         },
-        "-=1"
+        '-=1'
       )
     }
   }
@@ -241,51 +241,51 @@ const Logo = () => {
   return (
     <>
       <div
-        className="flex flex-col items-center"
+        className='flex flex-col items-center'
         style={{
-          rowGap: "1.2rem",
+          rowGap: '1.2rem',
         }}
       >
         <div
-          className="flex space-x-1 px-7 -ms-3 md:ms-0 justify-center items-center"
-          style={{ alignItems: "end" }}
+          className='flex space-x-1 px-7 -ms-3 md:ms-0 justify-center items-center'
+          style={{ alignItems: 'end' }}
         >
           {node.map((n) => (
             <span
               key={n.key}
               style={{
-                clipPath: "inset(-10px)",
+                clipPath: 'inset(-10px)',
               }}
             >
               {n}
             </span>
           ))}
         </div>
-        <div className="relative w-full">
+        <div className='relative w-full'>
           <Image
             ref={bannerBgRef}
             src={bannerBg}
-            alt="Ahmedabad-Banner"
-            layout="fill"
-            objectFit="cover"
-            className="px-3"
+            alt='Ahmedabad-Banner'
+            layout='fill'
+            objectFit='cover'
+            className='px-3'
             style={{
-              clipPath: "inset(0% 0% 100% 0%)",
+              clipPath: 'inset(0% 0% 100% 0%)',
             }}
           />
-          <div className="flex gap-x-1 scale-75 md:scale-100 left-[3px] top-[4px] absolute md:left-[72px] md:top-[12px]">
+          <div className='flex gap-x-1 scale-75 md:scale-100 left-[3px] top-[4px] absolute md:left-[72px] md:top-[12px]'>
             {ahmedabad.map((a, index) => (
               <div
                 key={a.key}
                 style={{
-                  clipPath: "inset(-10px)",
-                  width: "40px", // Fixed width for the container
-                  height: "40px", // Fixed height for the container
-                  display: "flex", // Center the image
-                  justifyContent: "center", // Horizontally center image
-                  alignItems: "center", // Vertically center image
-                  margin: index === 2 ? "5px 7px" : "0 0px",
-                  transform: index === 2 ? "translateX(2px)" : "translateX(0)",
+                  clipPath: 'inset(-10px)',
+                  width: '40px', // Fixed width for the container
+                  height: '40px', // Fixed height for the container
+                  display: 'flex', // Center the image
+                  justifyContent: 'center', // Horizontally center image
+                  alignItems: 'center', // Vertically center image
+                  margin: index === 2 ? '5px 7px' : '0 0px',
+                  transform: index === 2 ? 'translateX(2px)' : 'translateX(0)',
                 }}
               >
                 {a}
