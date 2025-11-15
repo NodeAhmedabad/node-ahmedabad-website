8e533a82950cc47388b4364df5d1a9237f6cebab
Nisharg Shah <nishargshah3101@gmail.com>
2025-11-16 01:05:21 +0530

    fix: event gallery

--- src/components/events/event/gallery/EventGallery.tsx
+++ src/components/events/event/gallery/EventGallery.tsx
@@ -1,6 +1,6 @@
 import Section from '@/app/Section';
 import ContentWrapper from '@/components/ContentWrapper';
-import GalleryGrid from '@/components/events/event/gallery/GalleryGrid';
+import EventGalleryGrid from '@/components/events/event/gallery/EventGalleryGrid';
 
 import type events from '@/data/events/events';
 import type { Component } from '@/types';
@@ -19,7 +19,7 @@ const EventGallery: Component<EventGalleryProps> = (props) => {
         description="Explore our vibrant community through photos from meetups, workshops, and events."
         title="Community Gallery"
       />
-      <GalleryGrid items={gallery} />
+      <EventGalleryGrid gallery={gallery} />
     </Section>
   );
 };
